e699cbcc13a3b37d98068d4aa2c3ad456acd45bc
Nick Dem <demeshkons@gmail.com>
2019-07-01 00:52:29 +0700

    Add the Web API Add the blank Web API

---
+++ DictionaryManager/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DictionaryManager.Startup))]
+
+namespace DictionaryManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
